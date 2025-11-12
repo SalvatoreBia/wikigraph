a25893d96dfe2b335001e10955adaae89935d010
PierpaoloSpadafora <pierpaolospadafora23+github@gmail.com>
2025-11-12 16:19:03 +0100

    dai lello

--- new_try/1_parse_file.sh
+++ new_try/1_parse_file.sh
@@ -1,6 +1,14 @@
 
-# extract page id and title from itwiki-latest-page.sql
+# scaricati i comandi ripgrep e xsv
 
+
+# extract page id and title from itwiki-latest-page.sql
 rg -oP "\(([0-9]+),0,'(\w+)',0," --replace '$1,$2' itwiki-latest-page.sql > id_name_pagemap.csv
 
 
+# extract from itwiki-latest-pagelinks.sql the source and target page ids
+rg -oP "\(([0-9]+),0,([0-9]+)" --replace '$1,$2' itwiki-latest-pagelinks.sql > id_to_id_pagemap.csv
+
+
+# Estrae solo la prima colonna (gli ID) da a.csv e la salva
+cut -d, -f1 a.csv > ids_validi.txt

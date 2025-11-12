86f7a16f83e7f9cb2ae2ccd48f25089767bb9548
PierpaoloSpadafora <pierpaolospadafora23+github@gmail.com>
2025-11-12 15:27:08 +0100

    prolollo

--- new_try/1_parse_file.sh
+++ new_try/1_parse_file.sh
@@ -1,4 +1,6 @@
 
 # extract page id and title from itwiki-latest-page.sql
 
-rg -o "^INSERT INTO.* VALUES \(([0-9]+),0,'(\w+)',0," --replace '$1,$2' itwiki-latest-page.sql > page_nodes.csv
+rg -oP "\(([0-9]+),0,'(\w+)',0," --replace '$1,$2' itwiki-latest-page.sql > id_name_pagemap.csv
+
+

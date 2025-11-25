b79c9f6a581fdba1dbf5b4a698b4b3c3f270b6cc
PierpaoloSpadafora <pierpaolospadafora23+github@gmail.com>
2025-11-25 02:14:49 +0100

    popi

--- src/1_download_wikipedia_files.sh
+++ src/1_download_wikipedia_files.sh
@@ -4,6 +4,7 @@
 DEST_DIR="../data"
 BASE_URL="https://dumps.wikimedia.org/itwiki/latest/"
 FILES=("itwiki-latest-page.sql.gz" "itwiki-latest-pagelinks.sql.gz")
+BZFILES=("itwiki-latest-pages-articles.xml.bz2")
 
 mkdir -p "$DEST_DIR"
 for F in "${FILES[@]}"; do
@@ -11,4 +12,10 @@ for F in "${FILES[@]}"; do
     wget -q --show-progress -P "$DEST_DIR" "$BASE_URL$F"
     echo "Estrazione $F..."
     gunzip -f "$DEST_DIR/$F"
+done
+for BF in "${BZFILES[@]}"; do
+    echo "Scaricamento $BF..."
+    wget -q --show-progress -P "$DEST_DIR" "$BASE_URL$BF"
+    echo "Estrazione $BF..."
+    bzip2 -d -f "$DEST_DIR/$BF"
 done
